c4748ceaade2c24f39d7d79ad28ca758e35baf1f
Сергей <90635069+Serjk3@users.noreply.github.com>
2022-02-06 13:52:45 +0300

    add homework 4

--- ht-4/src/App.tsx
+++ ht-4/src/App.tsx
@@ -3,8 +3,6 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Git } from "./components/Git";
 
-const Home = () => <div>HOME</div>;
-
 function App() {
   return (
     <div className="App">
